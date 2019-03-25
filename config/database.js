a1225d154c5f96be2bf100ae470be8e3f25378d7
UKISTU18 <arajegan.aj@gmail.com>
2019-03-25 11:37:19 +0530

    first commit

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// module.exports = {
+//
+//     'url' : 'mongodb://localhost:27017/nodeauth' // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+//
+// };
+//
+// module.exports = {
+//  url : "mongodb://admin:root0@ds161306.mlab.com:61306/notable"
+// };
